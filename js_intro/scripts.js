4424b02cc3fbd4968bfcddf1fd368a6a58fe49fb
Diana Vivar <vivad394@newschool.edu>
2016-10-19 21:19:33 +0000

    add home_page.html

---
+++ js_intro/scripts.js
@@ -0,0 +1,14 @@
+var age = 17;
+var parentsPres = false;
+
+if (age >= 18 || parentsPres == true) {
+    console.log("Come on in...");
+} else {
+    if (age == 17){
+        console.log("come back next year")
+    } 
+    else{
+    console.log("Come back when you are 18, or bring your parents...");
+}
+
+}
