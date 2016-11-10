42e39550249de985182ec0c58809074a64385c3b
Diana Vivar <vivad394@newschool.edu>
2016-11-10 19:27:18 +0000

    add comp_5.html

--- iterative_comp/comp_5.js
+++ iterative_comp/comp_5.js
@@ -1,4 +1,4 @@
-for (var dots = 0; dots < 100; dots++) {
+for (var dots = 0; dots <  120 ; dots++) {
     $(".wrapper").append("<div class='circle'></div>");
 }
 $(".circle").html("1");
@@ -8,22 +8,36 @@ $(".circle").each(function() {
    console.log(counter);
    $(this).html(counter);
     $(this).css("opacity",(counter/100));
-     $(this).css("height",(100+counter));
-     $(this).css("width",(100+counter));
-     $(this).css("font-size",(counter));
-     $(this).css("transform","rotate("+ (counter/100)*360 + "deg)");
-     $(this).css("top",Math.floor(Math.random()*$(window).height())); 
+    $(this).css("height",(100+counter));
+    $(this).css("width",(100+counter));
+    $(this).css("transform","rotate("+ (counter/120)*360 + "deg)");
+    $(this).css("top",Math.floor(Math.random()*$(window).height())); 
      $(this).css("left",Math.floor(Math.random()*$(window).width())); 
    counter++;
    //run this code
 });
-   $(".wrapper").click(function() {
-  //run this code when the mouse moves within the target
-     $("wrapper").css("color","blue");
+
+var hasClicked = false;
+var myColors = ["blue","red","lime","grey"];
+var lastColor = false;
+
+
+$(".circle").click(function() {
+    console.log(hasClicked);
+    
+    
+    hasClicked = true;
+    
+    var newColor = myColors[Math.floor(Math.random()*myColors.length)];
+    while (newColor == lastColor) {
+        newColor = myColors[Math.floor(Math.random()*myColors.length)];
+        console.log("getting new color...");
+    }
+    lastColor = newColor;
+    $(".circle").css("background-color",newColor);
+    
+    $(".circle").css("color","blue");
+    $(".body").css("background-color","red");
+  
 });
 
-$(".circle").mouseenter(function() {
-  //run this code when the mouse enters the target
-  if (click == true){
-    $(".circle").css("background-color","red")}
-});s
