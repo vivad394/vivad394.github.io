1a4c810885ff5bb6e81401a6c71b836c7cbe9753
Diana Vivar <vivad394@newschool.edu>
2016-12-15 14:07:47 +0000

    add home_page.html

--- homepage/home_page.js
+++ homepage/home_page.js
@@ -4,9 +4,18 @@ var myArray = ["sol.jpg", "mano.jpg", "apache.jpg", "rosa.jpg", "calavera.jpg",
 
 $(".deck").click(function() {
     console.log("you clicked it...");
-    var newCards = myArray[Math.floor(Math.random()*myArray.length)];
-    $(".deck").html('<img src="' + newCards + '" width="100%">');
- 
+    if (myArray.length > 0) {
+        //run our script if there are still items in our array
+        var randomCard = Math.floor(Math.random()*myArray.length);
+        var newCards = myArray[randomCard];
+        $(".deck").html('<img src="' + newCards + '" width="100%">');
+        //remove the chosen card from the array
+        myArray.splice(randomCard, 1);
+    } else {
+        // do something when there are no more cards in the array
+        $(".end-message").show();
+        
+    }
 });
 
 
