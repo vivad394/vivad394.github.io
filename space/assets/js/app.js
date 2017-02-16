a577dee89808b13297d13ef004891d1c567a65b5
Diana Vivar <vivad394@newschool.edu>
2017-02-16 10:40:03 -0500

    add flex-grid.html

---
+++ space/assets/js/app.js
@@ -0,0 +1,7 @@
+$(".wf").click(function() {
+	$(".step-container").append("<div class='someDiv'></div>");
+})
+
+$(".t").click(function() {
+	$(".step-container").append("<div class='someDiv'></div>");
+})
