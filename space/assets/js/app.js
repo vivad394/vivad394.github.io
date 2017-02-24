c31c523e8c7c671ab4bedf8316aceb81b270cd7d
Diana Vivar <vivad394@newschool.edu>
2017-02-23 21:25:04 -0500

    adding index.html

--- space/assets/js/app.js
+++ space/assets/js/app.js
@@ -1,7 +1,25 @@
-$(".wf").click(function() {
-	$(".step-container").hide("<div class='steps1 steps'></div>");
-})
+// var myArray = [$(' <div class="steps1 steps"></div>, <div class="steps2 steps"></div>, <div class="steps3 steps"></div>, <div class="steps4 steps"></div>, <div class="steps5 steps"></div>, <div class="steps6 steps"></div>')];
 
-$(".t").click(function() {
-	$(".step-container").show("<div class='someDiv'></div>");
-})
+// $(".wf").click(function() {
+// 	$(".step-container").show("<div class='steps1 steps'></div>");
+// })
+
+// if (myArray.length > 0) {
+//         //run our script if there are still items in our array
+//         var randomStep = Math.floor(Math.random()*myArray.length);
+//         var newStep = myArray[randomStep];
+
+//  myArray.splice(randomStep, 1);
+//     } else {
+//         $("").show();
+//         };
+
+// $(".wa").click(function() {
+// 	$(".step-container").append("<div class='steps2 steps'></div>");
+// })
+// $(".t").click(function() {
+// 	$(".step-container").append("<div class='someDiv'></div>");
+// })
+
+// $(".step-container").click(function() {(
+//     console.log("you clicked it...")
