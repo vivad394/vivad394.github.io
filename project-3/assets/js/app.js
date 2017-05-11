f2541054822db16df5959d67e83e0ce61d3eaefe
Diana Vivar <vivad394@newschool.edu>
2017-05-11 11:08:54 -0400

    adding project 3 files

--- project-3/assets/js/app.js
+++ project-3/assets/js/app.js
@@ -1,37 +1,53 @@
+// TRANSITION IN TO HOMEPAGE --------------------------------------------------------------
+
 setTimeout(function(){ 
 	$(".pick").fadeIn();
-}, 500);
+}, 1000);
+
+setTimeout(function(){
+	console.log("fading in");
+	$(".line").fadeIn();
+},2000);
 
 setTimeout(function(){ 
 	$(".choices").fadeIn();
-}, 1000);
+}, 500);
+
+// HOMEPAGE---------------------------------------------------------------------------------
 
 $(".one").click(function() {
 	var diceRoll = Math.floor(Math.random()*5);
 	if (diceRoll < 1) {
 		console.log("you made it");
-		$(".pick").hide();
-		$(".arrived").show();
-		$(".line").hide();
+		$(".pick").fadeOut();
+		$(".arrived").fadeIn();
+		$(".line").fadeOut();
+		$(".deported").hide();
 	} else {
 		console.log("try again...");
-		$(".pick").hide();
-		$(".deported").show();	
+		$(".pick").fadeOut();
+		$(".background-black").fadeIn();
+		$(".arrived").hide();
+		traveling();
 	}
-	$(".choices").hide();
+	$(".choices").fadeOut();
+
 });
 
 $(".two").click(function() {
 	var diceRoll = Math.floor(Math.random()*5);
 	if (diceRoll < 1) {
 		console.log("you made it");
-		$(".pick").hide();
-		$(".crossed").show();
-		$(".line").hide();
+		$(".pick").fadeOut();
+		$(".crossed").fadeIn();
+		$(".line").fadeOut();
+
 	} else {
 		console.log("try again...");
-		$(".pick").hide();
-		$(".caught").show();	
+		$(".pick").fadeOut();
+		$(".caught").fadeIn();
+		$(".background-black").fadeIn();
+		traveling2();
 	}
 	$(".choices").hide();
 });
@@ -40,13 +56,14 @@ $(".three").click(function() {
 	var diceRoll = Math.floor(Math.random()*5);
 	if (diceRoll < 1) {
 		console.log("you made it");
-		$(".pick").hide();
-		$(".swimming").show();
-		$(".line").hide();
+		$(".pick").fadeOut();
+		$(".swimming").fadeIn();
+		$(".line").fadeOut();
 	} else {
 		console.log("try again...");
-		$(".pick").hide();
-		$(".stopped").show();	
+		$(".pick").fadeOut();
+		$(".stopped").fadeIn();
+		$(".background-black").fadeIn();	
 	}
 	$(".choices").hide();
 });
@@ -55,18 +72,20 @@ $(".four").click(function() {
 	var diceRoll = Math.floor(Math.random()*5);
 	if (diceRoll < 1) {
 		console.log("you made it");
-		$(".pick").hide();
-		$(".flying").show();
-		$(".line").hide();
+		$(".pick").fadeOut();
+		$(".flying").fadeIn();
+		$(".line").fadeOut();
 	} else {
 		console.log("try again...");
-		$(".pick").hide();
-		$(".staying").show();	
+		$(".pick").fadeOut();
+		$(".staying").fadeIn();	
+		$(".background-black").fadeIn();
 	}
 	$(".choices").hide();
 });
 
 $(".back").click(function() {
+	$(".background-black").fadeOut();
 	$(".choices").show();
 	$(".pick").show();
 	$(".deported").hide();	
@@ -74,4 +93,112 @@ $(".back").click(function() {
 	$(".stopped").hide();
 	$(".staying").hide();
 
-})
+});
+
+
+function traveling() {
+	var traveling = setTimeout(function(){ 
+		$(".traveling").addClass('hide');
+		$(".deported").fadeIn();	
+	}, 1000);
+
+}
+
+function traveling2() {
+	var traveling = setTimeout(function(){ 
+		$(".traveling").addClass('hide');
+		$(".caught").fadeIn();	
+	}, 2000);
+
+}
+
+
+// JOBS--------------------------------------------------------------------------
+
+
+$(".uno").click(function() {
+	var diceRoll = Math.floor(Math.random()*10);
+	if (diceRoll < 5) {
+		console.log("you made it");
+		$(".pick-jobs").fadeOut();
+		$(".accepted").fadeIn();
+		$(".background-jobs").fadeIn();	
+	} else {
+		console.log("try again...");
+		$(".pick-jobs").fadeOut();
+		$(".rejected").fadeIn();
+}
+	$(".jobs").fadeOut();
+	$(".red").fadeOut();
+});
+
+$(".dos").click(function() {
+	var diceRoll = Math.floor(Math.random()*10);
+	if (diceRoll < 5) {
+		console.log("you made it");
+		$(".pick-jobs").fadeOut();
+		$(".cooking").fadeIn();
+		$(".background-jobs").fadeIn();
+	} else {
+		console.log("try again...");
+		$(".pick-jobs").fadeOut();
+		$(".waiting").fadeIn();
+}
+	$(".jobs").hide();{
+		console.log("im hiding...");}
+	$(".red").fadeOut();
+});
+
+$(".tres").click(function() {
+	var diceRoll = Math.floor(Math.random()*10);
+	if (diceRoll < 5) {
+		console.log("you made it");
+		$(".pick-jobs").fadeOut();
+		$(".limpiando").fadeIn();
+		$(".background-jobs").fadeIn();
+	} else {
+		console.log("try again...");
+		$(".pick-jobs").fadeOut();
+		$(".saliendo").fadeIn();
+}
+	$(".jobs").hide();{
+		console.log("im hiding...");}
+	$(".red").fadeOut();
+});
+
+$(".cuatro").click(function() {
+	var diceRoll = Math.floor(Math.random()*10);
+	if (diceRoll < 5) {
+		console.log("you made it");
+		$(".pick-jobs").fadeOut();
+		$(".constructing").fadeIn();
+		$(".background-jobs").fadeIn();
+	} else {
+		console.log("try again...");
+		$(".pick-jobs").fadeOut();
+		$(".not").fadeIn();
+}
+	$(".jobs").hide();{
+		console.log("im hiding...");}
+	$(".red").fadeOut();
+});
+
+$(".back2").click(function(){
+	$(".pick-jobs").fadeIn();
+	$(".jobs").fadeIn();
+	$(".rejected").fadeOut();
+	$(".waiting").fadeOut();
+	$(".saliendo").fadeOut();
+	$(".not").fadeOut();
+	if ($('.red').hasClass('clicked')) {
+		// dont do anything
+	} else {
+		$('.red').fadeIn();
+	}
+});
+
+$(".red").click(function(){
+	$(".red").hide().addClass('clicked');
+});
+
+
